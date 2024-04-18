181a930f714c58be7c589345a6c9169d28d33bf2
TOMINAGA HAYATO <tominaga_h@mad2007.co.jp>
2024-04-18 13:39:36 +0900

    test: write test of `TreeParser::parse_line`

--- src/git/parse.rs
+++ src/git/parse.rs
@@ -15,7 +15,7 @@ impl TreeParser {
         let mut tree_lines: Vec<&str> = self.tree.split('\n').collect();
         for line in tree_lines.iter_mut() {
             if line.is_empty() { continue; }
-            let parse_result = self.parse_line(line);
+            let parse_result = TreeParser::parse_line(line);
 			match parse_result {
 				Ok(item) => items.push(item),
 				Err(e) => return Err(e)
@@ -24,7 +24,7 @@ impl TreeParser {
         Ok(items)
     }
 
-    pub fn parse_line(&self, line: &mut &str) -> Result<TreeItem, ParseError> {
+    pub fn parse_line(line: &mut &str) -> Result<TreeItem, ParseError> {
         // line example::
         // 100644 blob ea8c4bf7f35f6f77f75d92ad8ce8349f6e81ddba    .gitignore
         let re = Regex::new(r"^\d+\s+blob\s+([a-f0-9]{40})\s+(.+)$").unwrap();
@@ -70,3 +70,49 @@ impl fmt::Display for ParseError {
 }
 
 impl std::error::Error for ParseError {}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn test_parse_line_normal() {
+		let mut test_case = "100644 blob ea8c4bf7f35f6f77f75d92ad8ce8349f6e81ddba    .gitignore";
+		let parse_result = TreeParser::parse_line(&mut test_case);
+		match parse_result {
+			Ok(item) => {
+				let hash_expected = "ea8c4bf7f35f6f77f75d92ad8ce8349f6e81ddba".to_string();
+				assert_eq!(hash_expected, item.hash);
+				let file_expected = ".gitignore".to_string();
+				assert_eq!(file_expected, item.file);
+			},
+			_ => panic!("Failed to parse line.")
+		}
+	}
+
+	#[test]
+	fn test_parse_line_abnormal_spaces() {
+		let mut test_case = "100644     blob	   ea8c4bf7f35f6f77f75d92ad8ce8349f6e81ddba .gitignore";
+		let parse_result = TreeParser::parse_line(&mut test_case);
+		match parse_result {
+			Ok(item) => {
+				let hash_expected = "ea8c4bf7f35f6f77f75d92ad8ce8349f6e81ddba".to_string();
+				assert_eq!(hash_expected, item.hash);
+				let file_expected = ".gitignore".to_string();
+				assert_eq!(file_expected, item.file);
+			},
+			_ => panic!("Failed to parse line.")
+		}
+	}
+
+	#[test]
+	#[should_panic]
+	fn test_parse_line_expect_error() {
+		let mut test_case = "ea8c4bf7f35f6f77f75d92ad8ce8349f6e81ddba";
+		let parse_result = TreeParser::parse_line(&mut test_case);
+		match parse_result {
+			Ok(_) => (),
+			Err(_) => panic!("failed")
+		}
+	}
+}
